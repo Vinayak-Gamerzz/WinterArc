d57137dd59be2e2331e5a145b97118b96be30a06
OG_Vinayak <vinayaktiwari307@gmail.com>
2026-09-13 08:51:41 +0530

    making script.js now cause im fed up from css ill do it later lawl

--- script.js
+++ script.js
@@ -5,8 +5,29 @@ window.addEventListener("load", () => {
 
     setTimeout(() => {
         laoder.classList.remove("loader-active");
-    }, 800);
+    }, 500);
 
     }, 200);
 
 });
+
+var CurrentTime="";
+
+    function timeUpdate() {
+        CurrentTime = new Date().toLocaleString();
+        var timeText = document.querySelector("#timebarelement");
+
+    if (timeText) {
+        timeText.innerHTML = CurrentTime;
+
+    }
+
+    var downloadLink = document.querySelector("#downloadLink");
+    if (downloadLink) {
+        var downloadName = "arcos_painting_" + CurrentTime.replace(/[^a-zA-z0-9,-]/g, "_") + ".png";
+        downloadLink.setAttribute("download", downloadName);
+
+    }
+
+    }
+    setInterval(timeUpdate, 1000);
