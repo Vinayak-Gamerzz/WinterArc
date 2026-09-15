7afd96bb64c2b457b440d919e2497458d1b2b2d8
OG_Vinayak <vinayaktiwari307@gmail.com>
2026-09-16 01:44:18 +0530

    added music player controls!

--- script.js
+++ script.js
@@ -2707,3 +2707,81 @@ function downloadPhoto(dataUrl) {
   link.click();
   document.body.removeChild(link);
 }
+
+const audioPlayer = document.querySelector(".audio-player");
+const audio = new Audio(
+  "https://stream1.rcast.net/73328"
+);
+ 
+console.dir(audio);
+ 
+audio.addEventListener(
+  "loadeddata",
+  () => {
+    audio.volume = .75;
+  },
+  false
+);
+ 
+ 
+ 
+const volumeSlider = audioPlayer.querySelector(".controls .volume-slider");
+volumeSlider.addEventListener('click', e => {
+  const sliderWidth = window.getComputedStyle(volumeSlider).width;
+  const newVolume = e.offsetX / parseInt(sliderWidth);
+  audio.volume = newVolume;
+  audioPlayer.querySelector(".controls .volume-percentage").style.width = newVolume * 100 + '%';
+}, false)
+ 
+setInterval(() => {
+  audioPlayer.querySelector(".time .current").textContent = getTimeCodeFromNum(
+    audio.currentTime
+  );
+}, 500);
+ 
+const playBtn = audioPlayer.querySelector(".controls .toggle-play");
+const radioimg = document.getElementById('radioimg');
+playBtn.addEventListener(
+  "click",
+  () => {
+    if (audio.paused) {
+      audio.src = 'https://stream1.rcast.net/73328';
+      playBtn.classList.remove("play");
+      playBtn.classList.add("pause");
+      audio.play();
+      radioimg.src = "./icons/radio-animation.gif"
+    } else {
+      playBtn.classList.remove("pause");
+      playBtn.classList.add("play");
+      audio.pause();
+      radioimg.src = "./icons/radio.png"
+      audio.src = ''
+    }
+  },
+  false
+);
+ 
+audioPlayer.querySelector(".volume-button").addEventListener("click", () => {
+  const volumeEl = audioPlayer.querySelector(".volume-container .volume");
+  audio.muted = !audio.muted;
+  if (audio.muted) {
+    volumeEl.classList.remove("icono-volumeMedium");
+    volumeEl.classList.add("icono-volumeMute");
+  } else {
+    volumeEl.classList.add("icono-volumeMedium");
+    volumeEl.classList.remove("icono-volumeMute");
+  }
+});
+ 
+function getTimeCodeFromNum(num) {
+  let seconds = parseInt(num);
+  let minutes = parseInt(seconds / 60);
+  seconds -= minutes * 60;
+  const hours = parseInt(minutes / 60);
+  minutes -= hours * 60;
+ 
+  if (hours === 0) return `${minutes}:${String(seconds % 60).padStart(2, 0)}`;
+  return `${String(hours).padStart(2, 0)}:${minutes}:${String(
+    seconds % 60
+  ).padStart(2, 0)}`;
+}
